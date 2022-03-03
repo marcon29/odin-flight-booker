5336250786a3ba6592b1b1239315bdbef2ea52f3
marcon29 <online@markstabler.com>
2022-03-03 00:30:23 -0700

    added assoc between Booking, Flight and Passenger

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,7 @@ class Flight < ApplicationRecord
 
     belongs_to :depart_airport, class_name: "Airport"
     belongs_to :arrive_airport, class_name: "Airport"
+    has_many :bookings
 
 
     def display_text
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,4 @@
 class Passenger < ApplicationRecord
+    has_many :bookings
+    
 end
